9569329df6623b7f02a118f6a81a38a5ead30b5b
tirumalareddysanampudi <45302057+tirumalareddysanampudi@users.noreply.github.com>
2024-04-01 19:38:48 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,5 @@
 pipeline{
 agent any 
- environment{
-   SCANNER_HOME=tool 'Sonar-Scanner'
- }
  stages{
  stage(' Clean Workspace'){
   steps{
@@ -14,28 +11,6 @@ agent any
     git branch: 'main', changelog: false, credentialsId: 'Git-Hub', poll: false, url: 'https://github.com/tirumalareddysanampudi/Geetha-school-2010.git'
   }
  }
-  stage('mvn Compile'){
-  steps{
-    sh "mvn compile"
-  }
- }
-  stage('mvn Test'){
-  steps{
-    sh "mvn test"
-  }
- }
-  stage('TRIVY FS SCAN Filesystem Scan '){
-  steps{
-    sh "trivy fs /root/.jenkins/workspace/School-pipeline -o trivy-fsreport.html"
-  }
- }
- stage('SonarQube Analysis'){
- steps{
- withSonarQubeEnv('sonar-server') {
-    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=GeethaGurukulam \
-                    -Dsonar.projectKey=Jenkins '''
-}
- }
- }
+ 
 }
 }
